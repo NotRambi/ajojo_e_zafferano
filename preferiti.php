b70ca6f470bce1a16c3ade961880269aafa05fdc
Federico Angarella <164171052+fortigate3600@users.noreply.github.com>
2024-05-03 21:48:20 +0200

    funzione chiamata in ricettario

---
+++ preferiti.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $mettiORtogli=$_POST["mettiORtogli"];
+    $ricetta = $_POST["ricetta"];
+    $user=$_SESSION['user'];
+    $dbconn = pg_connect("host=localhost port=5432 dbname=ajojo user=postgres password=biar");
+
+    if($mettiORtogli=="metti"){
+        $query="insert into preferiti values ('$user','$ricetta')";
+    }
+    if($mettiORtogli=="togli"){
+        $query="DELETE FROM preferiti where username='$user' AND ricetta='$ricetta'";
+        pg_query($dbconn,$query);
+    }
+
+    
+    pg_close($dbconn);
+
+} else {
+    // Handle invalid requests or no data sent
+    echo "Invalid request!";
+}
+?>
